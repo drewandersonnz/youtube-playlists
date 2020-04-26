7e17240256ad99c8665d5d8fe57f492be9993cc8
Drew Anderson <drew@onlycentral.com>
2020-04-26 13:08:08 +0200

    violentmonkey script

---
+++ js/monkey.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Youtube Tools
+// @version      0.1
+// @description  Youtube Tools script injection
+// @author       Drew Anderson
+// @namespace    https://youtube.com/
+// @include      /https?:\/\/youtube\.com/watch.*/
+// @include      /https?:\/\/www\.youtube\.com/watch.*/
+// ==/UserScript==
+
+var src = "https://www.drewanderson.org/youtube-playlists/js/tools.js";
+//src = "http://127.0.0.1:8080/js/tools.js";
+
+var script   = document.createElement("script");
+script.type  = "text/javascript";
+script.src   = src    // use this for linked script
+
+var head = document.head || document.getElementsByTagName('head')[0];
+head.insertBefore(script, head.lastChild);
