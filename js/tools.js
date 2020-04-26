6b1e3a15eae851ec26d209eff261a14efab8f17f
Drew Anderson <drew@onlycentral.com>
2020-04-26 13:13:49 +0200

    playlistVideosRemove so it auto-finds in console

--- js/tools.js
+++ js/tools.js
@@ -8,7 +8,7 @@ function getVideoRemoveButton() {
     return document.querySelector('ytd-playlist-panel-video-renderer.style-scope:nth-child(1) > div:nth-child(2) > ytd-menu-renderer:nth-child(1) > div:nth-child(1) > ytd-button-renderer:nth-child(1) > a:nth-child(1) > yt-icon-button:nth-child(1)');
 }
 
-async function removeVideos(count, delay = 0.4) {
+async function playlistVideosRemove(count, delay = 0.4) {
     for (var i = 0; i < count; i++) {
         console.log("removing video " + i + " of " + count);
 
