fb7eed343510f13d9096e2fb5b991ae9a51be032
Drew Anderson <drew@onlycentral.com>
2020-04-26 13:14:01 +0200

    homepage / download / support urls

--- js/monkey.js
+++ js/monkey.js
@@ -3,6 +3,9 @@
 // @version      0.1
 // @description  Youtube Tools script injection
 // @author       Drew Anderson
+// @homepageURL  https://www.drewanderson.org/youtube-playlists/
+// @downloadURL  https://www.drewanderson.org/youtube-playlists/js/monkey.js
+// @supportURL   https://github.com/drewandersonnz/youtube-playlists/
 // @namespace    https://youtube.com/
 // @include      /https?:\/\/youtube\.com/watch.*/
 // @include      /https?:\/\/www\.youtube\.com/watch.*/
