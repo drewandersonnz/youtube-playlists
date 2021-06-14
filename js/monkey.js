be578fae9be0e03ac45d49db17085b88be6775ad
Drew Anderson <drewandersonnz@users.noreply.github.com>
2021-06-14 14:21:11 +0200

    new version

--- js/monkey.js
+++ js/monkey.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Youtube Tools
-// @version      0.1
+// @version      0.202106
 // @description  Youtube Tools script injection
 // @author       Drew Anderson
 // @icon         https://s.ytimg.com/yts/img/favicon_32-vflOogEID.png
